31aacebe6058ebbeb7038158bdc8e50e047d4344
Hope Barrow <barr.writing@gmail.com>
2022-08-05 17:47:54 -0500

    debugging

--- server.js
+++ server.js
@@ -1,75 +1,12 @@
 const PORT = process.env.PORT || 3001;
-const fs = require ('fs');
-const path = require('path');
+
 const express = require('express');
 
-const app = express(); 
-
-const theNotes =('./db/db.json');
+const app = express();
 
 app.use(express.urlencoded({ extended:true }));
 app.use(express.json());
 app.use(express.static('public'));
-
-app.get('/api/notes', (req, res) => {
-    res.json(theNotes.slice(1));
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(path.json(__dirname, './public/index.html'));
-});
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.json(__dirname, './public/notes.html'));
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-function noteCreation(body, notesArray) {
-    const newNote = body;
-        if (!Array.isArray(notesArray))
-            notesArray= [];
-
-        if (notesArray.length === 0)
-                notesArray.push(0);
-
-                body.id=notesArray[0];
-                    notesArray[0]++;
-
-            notesArray.push(newNote);
-                fs.writeFileSync(
-                    path.join(__dirname, './db/db.json'),
-                    JSON.stringify(notesArray, null, 2)
-
-                );
-                    return newNote
-                };
-
-
-app.post('api/notes', (req, res) => {
-    const newNote = noteCreation(req.body, theNotes);
-    res.json(newNote)
-});
-
-function noteDeletion(id, notesArray) {
-    for (let i = 0; i < notesArray.length; i++) {
-        let note = notesArray[i];
-
-        if (note.id === id) {
-            notesArray.splice(i, 1);
-            fs.writeFileSync(
-                path.join(__dirname, './db/db.json'),
-                JSON.stringify(notesArray, null, 2)
-            )
-            break;
-        }}};
-
-app.delete('/api/notes/:id', (req, res) => {
-    noteDeletion(req.params.id, theNotes);
-    res.json(true);
-});
 
 app.listen(PORT, () => {
     console.log(`API server is now port on ${PORT}`);
