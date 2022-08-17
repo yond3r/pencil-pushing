7977c4c8fa02998bc77e1b0d9b31a59b857f27fa
Hope Barrow <barr.writing@gmail.com>
2022-08-17 16:54:06 -0500

    attempting to debug

--- server.js
+++ server.js
@@ -24,9 +24,9 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, './public/index.html'));
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 function noteCreation(body, notesArray) {
     const newNote = body;
