ef570c046ed9c047425575660216da355dae5e45
Hope Barrow <barr.writing@gmail.com>
2022-08-17 12:46:33 -0500

    fixed my nerd mistakes

--- server.js
+++ server.js
@@ -18,11 +18,11 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.json(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.json(__dirname, './public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 app.get('*', (req, res) => {
