763ddf3d1a67fa8adb2a862c90222a2ec5cbb1ce
Hope Barrow <barr.writing@gmail.com>
2022-08-17 16:48:00 -0500

    file removal

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 // app.get('*', (req, res) => {
