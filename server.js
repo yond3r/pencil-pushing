a1f8e209c22c5c885978a27086f32a8a0b3f487f
Hope Barrow <barr.writing@gmail.com>
2022-08-17 22:21:38 -0500

    still getting a really silly 404 but I'm just rolling w/ it for now

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const PORT = process.env.PORT || 3001;
-const fs = require ('fs');
+const fs = require('fs');
 const path = require('path');
+const { v4:uuid } = require('uuid');
 const express = require('express');
 
 const app = express();
 
-const theNotes =('./db/db.json');
+const theNotes = ('./db/db.json');
 
-app.use(express.urlencoded({ extended:true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
@@ -24,34 +25,39 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '/public/index.html'));
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
 function noteCreation(body, notesArray) {
     const newNote = body;
-        if (!Array.isArray(notesArray))
-            notesArray= [];
+    if (!Array.isArray(notesArray))
+        notesArray = [];
 
-        if (notesArray.length === 0)
-                notesArray.push(0);
+    if (notesArray.length === 0)
+        notesArray.push(0);
 
-                body.id = notesArray[0];
-                    notesArray[0]++;
+    body.id = notesArray[0];
+    notesArray[0]++;
 
-            notesArray.push(newNote);
-                fs.writeFile(
-                    path.join(__dirname, './db/db.json'),
-                    JSON.stringify(notesArray, null, 2)
-
-                );
-                    return newNote
-                };
+    notesArray.push(newNote);
+    fs.writeFile(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2));
+            return newNote
+};
 
 
-app.post('api/notes', (req, res) => {
-    const newNote = noteCreation(req.body, theNotes);
-    res.json(newNote)
+app.post('api/notes/:id', (req, res) => {
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) {
+            throw err;
+        } else {
+            const newNote = noteCreation(req.body.id = uuid(), theNotes);
+            res.json(newNote)
+
+        }
+    })
 });
 
 function noteDeletion(id, notesArray) {
@@ -65,7 +71,9 @@ function noteDeletion(id, notesArray) {
                 JSON.stringify(notesArray, null, 2)
             )
             break;
-        }}};
+        }
+    }
+};
 
 app.delete('/api/notes/:id', (req, res) => {
     noteDeletion(req.params.id, theNotes);
